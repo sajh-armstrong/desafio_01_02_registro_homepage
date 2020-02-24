2d8099fcab1c0a8284dce451122243fa9571f8d6
Sajh Armstrong <sajharmstrong@gmail.com>
2020-02-24 17:04:23 -0300

    added register button and activity

---
+++ app/src/main/java/com/desafiolatam/desafio_01_01_login/ActivityRegister.java
@@ -0,0 +1,14 @@
+package com.desafiolatam.desafio_01_01_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
